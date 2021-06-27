80e3b5938711256195cbd3b22f0a61f5bd9839e3
benacq <benacq44@gmail.com>
2021-06-27 23:57:09 +0000

    shell sort

--- selection sort/main.cpp
+++ selection sort/main.cpp
@@ -3,14 +3,26 @@
 
 
 std::vector<int> selectionSort(std::vector<int> data) {
+
+
     for (int i = 0; i < data.size(); ++i) {
+
+        std::cout << "Pass " << i << ": " << std::endl;
         int minIndex = i;
+
         for (int j = i + 1; j < data.size(); ++j) {
             if (data[j] < data[minIndex]) {
                 minIndex = j;
             }
         }
         std::swap(data[i], data[minIndex]);
+
+        for (int j : data) {
+            std::cout << j << " ";
+        }
+        std::cout << "\n";
+
+
     }
 
     return data;
@@ -18,9 +30,12 @@ std::vector<int> selectionSort(std::vector<int> data) {
 
 int main() {
 //    std::cout << "Hello, World!" << std::endl;
-    std::vector<int> data = selectionSort({37, 525, 283, 766, 5, 7, 1, 8, 9, 21});
-    for (int i : data) {
-        std::cout << i << " ";
-    }
+    std::vector<int> nums = {128, 47, 8521, 186, 774, 8975, 741, 121, 100, 500, 7789, 7410};
+    std::vector<int> data = selectionSort(nums);
+
+//    for (int i : data) {
+//        std::cout << i << " ";
+//    }
+
     return 0;
 }
