861b108e2a07b08092ebad6a4e2f0bf6111f68fd
benacq <benacq44@gmail.com>
2021-09-07 07:03:36 +0000

    updates

--- trees/binary trees/main.cpp
+++ trees/binary trees/main.cpp
@@ -20,13 +20,40 @@ public:
     static Node *insert(Node **root, int data) {
         if (*root == nullptr) {
             *root = createNode(data);
-
         } else if (data <= (*root)->data) {
             (*root)->left = insert(&(*root)->left, data);
         } else {
             (*root)->right = insert(&(*root)->right, data);
         }
         return (*root);
+    }
+
+
+public:
+    static void infix(Node **root) {
+        if (*root == nullptr) return;
+        infix(&(*root)->left);
+        printf("%d ", (*root)->data);
+        infix(&(*root)->right);
+
+    }
+
+public:
+    static void prefix(Node **root) {
+        if (*root == nullptr) return;
+        printf("%d ", (*root)->data);
+        prefix(&(*root)->left);
+        prefix(&(*root)->right);
+
+    }
+
+public:
+    static void postfix(Node **root) {
+        if (*root == nullptr) return;
+        postfix(&(*root)->left);
+        postfix(&(*root)->right);
+        printf("%d ", (*root)->data);
+
     }
 
 
@@ -49,15 +76,17 @@ public:
 int main() {
     struct Node *root;
     root = nullptr;
-    BinaryTree::insert(&root, 2);
     BinaryTree::insert(&root, 22);
+    BinaryTree::insert(&root, 2);
     BinaryTree::insert(&root, 42);
 
+    BinaryTree::infix(&root);
+
 //    std::cout << BinaryTree::min(&root);
-    if (BinaryTree::search(&root, 221)) {
-        std::cout << "Found";
-    } else {
-        std::cout << "Not found";
-    }
+//    if (BinaryTree::search(&root, 221)) {
+//        std::cout << "Found";
+//    } else {
+//        std::cout << "Not found";
+//    }
     return 0;
 }
